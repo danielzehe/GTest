309e2b1b6b7915143a4ab0929b02d073aefed2c6
Daniel Zehe <signup@daniel-zehe.de>
2017-12-22 13:16:43 +0800

    breaking 1 test

--- tests.cpp
+++ tests.cpp
@@ -5,7 +5,7 @@ TEST(SquareRootTest, PositiveNos) {
     EXPECT_EQ(6, squareRoot(36.0));
     EXPECT_EQ(18.0, squareRoot(324.0));
     EXPECT_EQ(25.4, squareRoot(645.16));
-    EXPECT_EQ(0, squareRoot(0.0));
+    EXPECT_EQ(1, squareRoot(0.0));
 }
 
 TEST(SquareRootTest, NegativeNos) {
